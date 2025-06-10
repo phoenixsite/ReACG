3ca637f4217cc06a8e0fd01f322af418b699c810
Carlos Romero Cruz <phoenixsitegit@outlook.com>
2025-06-10 08:59:46 +0200

    Uncommented execution

--- src/generate_aes.sh
+++ src/generate_aes.sh
@@ -8,40 +8,37 @@ NTHREADS=4
 
 for config_file in ../configs/transferability/acg*.yaml
 do
-	if [ $config_file == "../configs/transferability/acg_resnet50_eps1.yaml" ] || [ $config_file == "../configs/transferability/acg_vgg16_eps1.yaml" ] || [ $config_file == "../configs/transferability/acg_vgg16_eps2.yaml" ]; then
-		echo "Executing ACG with $config_file"
-		uv run $PROGRAM -p $config_file \
-			-g 0 \
-			-o $RESULTS_DIR \
-			--log_level $LOG_LEVEL \
-			--save-adversarial \
-			--n_threads $NTHREADS \
-			--cmd_param batch_size:int:$BATCH_SIZE
-	fi
+	echo "Executing ACG with $config_file"
+	uv run $PROGRAM -p $config_file \
+		-g 0 \
+		-o $RESULTS_DIR \
+		--log_level $LOG_LEVEL \
+		--save-adversarial \
+		--n_threads $NTHREADS \
+		--cmd_param batch_size:int:$BATCH_SIZE
 done
 
-#for config_file in ../configs/transferability/apgd*.yaml
-#do
-#	echo  "Executing APGD with $config_file"
-#	uv run $PROGRAM -p $config_file \
-#		-g 0 \
-#		-o $RESULTS_DIR \
-#		--log_level $LOG_LEVEL \
-#		--save-adversarial \
-#		--n_threads $NTHREADS \
-#		--cmd_param batch_size:int:$BATCH_SIZE
-#done
+for config_file in ../configs/transferability/apgd*.yaml
+do
+	echo  "Executing APGD with $config_file"
+	uv run $PROGRAM -p $config_file \
+		-g 0 \
+		-o $RESULTS_DIR \
+		--log_level $LOG_LEVEL \
+		--save-adversarial \
+		--n_threads $NTHREADS \
+		--cmd_param batch_size:int:$BATCH_SIZE
+done
 
 for config_file in ../configs/transferability/reacg*.yaml
 do
-	if [ $config_file == "../configs/transferability/reacg_resnet50_eps1.yaml" ] || [ $config_file == "../configs/transferability/reacg_vgg16_eps1.yaml" ] || [ $config_file == "../configs/transferability/reacg_vgg16_eps2.yaml" ]; then
-		echo "Executing ReACG with $config_file"
-		uv run $PROGRAM -p $config_file \
-			-g 0 \
-			-o $RESULTS_DIR \
-			--log_level $LOG_LEVEL \
-			--save-adversarial \
-			--n_threads $NTHREADS \
-			--cmd_param batch_size:int:$BATCH_SIZE
-	fi
+	echo "Executing ReACG with $config_file"
+	uv run $PROGRAM -p $config_file \
+		-g 0 \
+		-o $RESULTS_DIR \
+		--log_level $LOG_LEVEL \
+		--save-adversarial \
+		--n_threads $NTHREADS \
+		--cmd_param batch_size:int:$BATCH_SIZE
+
 done
